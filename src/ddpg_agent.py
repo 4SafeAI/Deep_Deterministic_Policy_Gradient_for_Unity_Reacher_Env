a3cae2cd95b09238587cb9a6993b8c7bf54e663a
Tobias Latka <Tobias.Latka@gmx.de>
2020-04-26 23:10:42 +0200

    *Refactor*: Refactor code to ease understanding.

--- src/ddpg_agent.py
+++ src/ddpg_agent.py
@@ -1,6 +1,4 @@
 import copy
-import random
-from collections import namedtuple, deque
 
 import numpy as np
 import torch
@@ -9,7 +7,6 @@ import torch.optim as optim
 
 from src.model import Actor, Critic
 from src.replay_buffer import ReplayBuffer
-
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
@@ -24,19 +21,19 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent(object):
     """Interacts with and learns from the environment."""
-    
     def __init__(self, state_size, action_size, random_seed):
         """Initialize an Agent object.
         
-        Params
-        ======
+        Args:
             state_size (int): dimension of each state
+
             action_size (int): dimension of each action
+
             random_seed (int): random seed
         """
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(random_seed)
+        self.seed = np.random.seed(random_seed)
 
         # Actor Network (w/ Target Network)
         self.actor_local = Actor(state_size, action_size, random_seed).to(device)
@@ -52,17 +49,14 @@ class Agent(object):
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE, random_seed, device)
     
     def step(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
-        # Save experience / reward
         self.memory.add(state, action, reward, next_state, done)
-
-        # Learn, if enough samples are available in memory
         if len(self.memory) > BATCH_SIZE:
             experiences = self.memory.sample()
-            self.learn(experiences, GAMMA)
+            self.learn(experiences)
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -78,55 +72,63 @@ class Agent(object):
     def reset(self):
         self.noise.reset()
 
-    def learn(self, experiences, gamma):
-        """Update policy and value parameters using given batch of experience tuples.
-        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
-        where:
-            actor_target(state) -> action
-            critic_target(state, action) -> Q-value
+    def learn(self, experiences):
+        """Update policy model and value model parameters using given batch of experience tuples.
 
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+        Note:
+            Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+
+            where:
+
+                actor_target(state) -> action
+
+                critic_target(state, action) -> Q-value
+
+        Args:
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
+
             gamma (float): discount factor
         """
-        states, actions, rewards, next_states, dones = experiences
+        self._update_critic_network(experiences)
+        self._update_actor_network(experiences)
+        self._update_target_networks()
 
-        # ---------------------------- update critic ---------------------------- #
+    def _update_critic_network(self, experiences):
+        states, actions, rewards, next_states, dones = experiences
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
-        # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-        # Compute critic loss
+        Q_targets = rewards + (GAMMA * Q_targets_next * (1 - dones))
+        # Compute and minimize critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
-        # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        # ---------------------------- update actor ---------------------------- #
-        # Compute actor loss
+    def _update_actor_network(self, experiences):
+        states, actions, rewards, next_states, dones = experiences
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
-        # Minimize the loss
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
 
-        # ----------------------- update target networks ----------------------- #
+    def _update_target_networks(self):
         self.soft_update(self.critic_local, self.critic_target, TAU)
         self.soft_update(self.actor_local, self.actor_target, TAU)                     
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
 
-        Params
-        ======
+        Note:
+            θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Args:
             local_model: PyTorch model (weights will be copied from)
+
             target_model: PyTorch model (weights will be copied to)
+
             tau (float): interpolation parameter 
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
@@ -135,13 +137,24 @@ class Agent(object):
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
-
     def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
-        """Initialize parameters and noise process."""
+        """Initialize parameters and noise process.
+
+        Args:
+            size: size of the random variable (noise).
+
+            seed: random seed.
+
+            mu: mean reversion level (equilibrium position).
+
+            theta: mean reversion rate (rigidity of Ornstein-Uhlenbeck process).
+
+            sigma: diffusion (impact of randomness on outcome of the process).
+        """
         self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
-        self.seed = random.seed(seed)
+        self.seed = np.random.seed(seed)
         self.reset()
 
     def reset(self):
@@ -151,6 +164,6 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.rand(len(x))
         self.state = x + dx
         return self.state
--- src/model.py
+++ src/model.py
@@ -5,37 +5,26 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def load_model(filename):
-    """Loads a model from file and returns a QNetwork object."""
+def load_actor(filename, seed=0):
+    """Loads a model from file and returns a Actor model object."""
     checkpoint = torch.load(filename)
-    model = QNetwork(checkpoint['input_size'], checkpoint['output_size'], checkpoint['hidden_layers'])
-    model.load_state_dict(checkpoint['state_dict'])
-    return model
+    state_size = checkpoint['state_size']
+    action_size = checkpoint['action_size']
+    hidden_sizes = checkpoint['hidden_sizes']
+    actor = Actor(state_size, action_size, seed, *hidden_sizes)
+    actor.load_state_dict(checkpoint['state_dict'])
+    return actor
 
 
-def save_model(model, filename='model.pt'):
-    """Saves a model to file (architecture + state_dict (weights)).
-
-    Params
-    ======
-        filename (str): model filename (defaults to "model.pt")
-    """
-    checkpoint = {
-        'input_size': model.linear_layers[0].in_features,
-        'output_size': model.linear_layers[-1].out_features,
-        'hidden_layers': [layer.out_features for layer in model.linear_layers[:-1]],
-        'state_dict': model.state_dict()}
-    torch.save(checkpoint, filename)
-
-
-def save_checkpoint(model, filename='checkpoint.pth'):
-    """Saves the model weights to file only.
-
-    Params
-    ======
-        filename (str): checkpoint filename (defaults to "checkpoint.pt")
-    """
-    torch.save(model.state_dict(), filename)
+def load_critic(filename, seed=0):
+    """Loads a model from file and returns a Critic model object."""
+    checkpoint = torch.load(filename)
+    state_action_size = checkpoint['state_action_size']
+    value_size = checkpoint['value_size']
+    hidden_sizes = checkpoint['hidden_sizes']
+    actor = Critic(state_action_size, value_size, seed, *hidden_sizes)
+    actor.load_state_dict(checkpoint['state_dict'])
+    return actor
 
 
 def hidden_init(layer):
@@ -43,17 +32,21 @@ def hidden_init(layer):
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
-class Actor(nn.Module):
-    """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+class Actor(nn.Module):
+    """Actor (Deterministic Policy) Model."""
+    def __init__(self, state_size, action_size, seed, fc1_units=40, fc2_units=20):
         """Initialize parameters and build model.
-        Params
-        ======
+
+        Args:
             state_size (int): Dimension of each state
+
             action_size (int): Dimension of each action
+
             seed (int): Random seed
+
             fc1_units (int): Number of nodes in first hidden layer
+
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
@@ -74,35 +67,80 @@ class Actor(nn.Module):
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
 
+    def save_model(self, filename='model_actor.pt'):
+        """Saves model to file (architecture + state_dict (weights)).
+
+        Args:
+            filename (str): model filename (defaults to "model_actor.pt")
+        """
+        checkpoint = {
+            'state_size': self.fc1.in_features,
+            'action_size': self.fc3.out_features,
+            'hidden_sizes': [layer.out_features for layer in [self.fc1, self.fc2]],
+            'state_dict': self.state_dict()}
+        torch.save(checkpoint, filename)
+
+    def save_checkpoint(self, filename='checkpoint_actor.pth'):
+        """Saves model weights to file only.
+
+        Args:
+            filename (str): checkpoint filename (defaults to "checkpoint_actor.pt")
+        """
+        torch.save(self.state_dict(), filename)
+
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
-
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fc1_units=40, fc2_units=20):
         """Initialize parameters and build model.
-        Params
-        ======
+
+        Args:
             state_size (int): Dimension of each state
+
             action_size (int): Dimension of each action
+
             seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
+
+            fc1_units (int): Number of nodes in the first hidden layer
+
             fc2_units (int): Number of nodes in the second hidden layer
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
-        x = torch.cat((xs, action), dim=1)
+        x = torch.cat((state, action), dim=1)
+        x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+    def save_model(self, filename='model_critic.pt'):
+        """Saves model to file (architecture + state_dict (weights)).
+
+        Args:
+            filename (str): model filename (defaults to "model_critic.pt")
+        """
+        checkpoint = {
+            'state_action_size': self.fc1.in_features,
+            'value_size': self.fc3.out_features,
+            'hidden_sizes': [layer.out_features for layer in [self.fc1, self.fc2]],
+            'state_dict': self.state_dict()}
+        torch.save(checkpoint, filename)
+
+    def save_checkpoint(self, filename='checkpoint_critic.pth'):
+        """Saves model weights to file only.
+
+        Args:
+            filename (str): checkpoint filename (defaults to "checkpoint_critic.pt")
+        """
+        torch.save(self.state_dict(), filename)
